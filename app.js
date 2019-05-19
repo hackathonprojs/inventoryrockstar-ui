da53cff0e6d325bee29ea287a8c180f6517b4ced
swswsw <apiswswsw@gmail.com>
2019-05-18 21:29:46 -0700

    allow set/read of inventoryAmount on backend

--- app.js
+++ app.js
@@ -6,4 +6,19 @@ const port = 8000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// store a value (number of inventory)
+// has a simple api to set that value.
+// number of inventory
+let inventoryAmount = 4;
+
+app.get('/inventoryAmount', (req, res) => {
+  // if newValue is provided, set the newvalue.
+  if (req.query.newValue) {
+    inventoryAmount = parseInt(req.query.newValue);
+  }
+
+  res.send(inventoryAmount.toString());
+  res.end();
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
